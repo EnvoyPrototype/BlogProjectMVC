7a7811038aa723200b42e40b3d0e7e1862ddacf6
Jim Rogers <thejimrogers@pm.me>
2021-10-24 07:13:44 -0500

    deleted views/tags/details

--- Controllers/TagsController.cs
+++ Controllers/TagsController.cs
@@ -20,24 +20,24 @@ namespace BlogProjectMVC.Controllers
         }
 
         // GET: Tags/Details/5
-        public async Task<IActionResult> Details(int? id)
-        {
-            if (id == null)
-            {
-                return NotFound();
-            }
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            var tag = await _context.Tags
-                .Include(t => t.BlogUser)
-                .Include(t => t.Post)
-                .FirstOrDefaultAsync(m => m.Id == id);
-            if (tag == null)
-            {
-                return NotFound();
-            }
+        //    var tag = await _context.Tags
+        //        .Include(t => t.BlogUser)
+        //        .Include(t => t.Post)
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (tag == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return View(tag);
-        }
+        //    return View(tag);
+        //}
 
         // GET: Tags/Create
         //public IActionResult Create()
