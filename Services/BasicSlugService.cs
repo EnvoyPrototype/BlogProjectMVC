8f87bfea40130c2d2ccdad43b4ff0c12408f35e1
Jim Rogers <thejimrogers@pm.me>
2021-10-26 16:33:41 -0500

    starting basic slug service

---
+++ Services/BasicSlugService.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProjectMVC.Services
+{
+    public class BasicSlugService : ISlugService
+    {
+        public bool IsUnique(string slug)
+        {
+            throw new NotImplementedException();
+        }
+
+        public string UrlFriendly(string title)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ Services/ISlugService.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BlogProjectMVC.Services
+{
+    public interface ISlugService
+    {
+        string UrlFriendly(string title);
+        bool IsUnique(string slug);
+    }
+}
