f0421e7ee4dae532e2d4fb7649c2c49d6a6786d7
Jim Rogers <thejimrogers@pm.me>
2021-10-29 12:23:19 -0500

    configure "resend email verification"

--- Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -47,6 +47,23 @@ namespace BlogProjectMVC.Areas.Identity.Pages.Account
 
         public class InputModel
         {
+
+            [Required]
+            [Display(Name = "First Name")]
+            [StringLength(50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long.", MinimumLength = 2)]
+            public string FirstName { get; set; }
+
+            [Required]
+            [Display(Name = "Last Name")]
+            [StringLength(50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long.", MinimumLength = 2)]
+            public string LastName { get; set; }
+
+            // DELETE THIS?
+            // [Required]
+            // [Display(Name = "Display Name")]
+            // [StringLength(50, ErrorMessage = "The {0} must be at least {2} and no more than {1} characters long.", MinimumLength = 2)]
+            // public string DisplayName { get; set; }
+
             [Required]
             [EmailAddress]
             [Display(Name = "Email")]
@@ -76,7 +93,14 @@ namespace BlogProjectMVC.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
             if (ModelState.IsValid)
             {
-                var user = new BlogUser { UserName = Input.Email, Email = Input.Email };
+                var user = new BlogUser { 
+                    FirstName = Input.FirstName,
+                    LastName = Input.LastName,
+                    // DisplayName = Input.DisplayName,
+                    UserName = Input.Email, 
+                    Email = Input.Email 
+                };
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
