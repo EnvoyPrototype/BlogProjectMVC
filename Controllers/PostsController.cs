89bf7b6e169bc78cd80f4d789d05053ea2b298a0
Jim Rogers <thejimrogers@pm.me>
2021-11-13 08:54:30 -0600

    updated Details action in PostsController to take string slug rather than int id. updated Posts/Index.cshtml details button to route slug. "custom model errors" was the commit before custom routing.

--- Controllers/PostsController.cs
+++ Controllers/PostsController.cs
@@ -36,9 +36,29 @@ namespace BlogProjectMVC.Controllers
         }
 
         // GET: Posts/Details/5
-        public async Task<IActionResult> Details(int? id)
+        //public async Task<IActionResult> Details(int? id)
+        //{
+        //    if (id == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    var post = await _context.Posts
+        //        .Include(p => p.Blog)
+        //        .Include(p => p.BlogUser)
+        //        .Include(p => p.Tags)
+        //        .FirstOrDefaultAsync(m => m.Id == id);
+        //    if (post == null)
+        //    {
+        //        return NotFound();
+        //    }
+
+        //    return View(post);
+        //}
+
+        public async Task<IActionResult> Details(string slug)
         {
-            if (id == null)
+            if (string.IsNullOrEmpty(slug))
             {
                 return NotFound();
             }
@@ -47,12 +67,11 @@ namespace BlogProjectMVC.Controllers
                 .Include(p => p.Blog)
                 .Include(p => p.BlogUser)
                 .Include(p => p.Tags)
-                .FirstOrDefaultAsync(m => m.Id == id);
+                .FirstOrDefaultAsync(m => m.Slug == slug);
             if (post == null)
             {
                 return NotFound();
             }
-
             return View(post);
         }
 
